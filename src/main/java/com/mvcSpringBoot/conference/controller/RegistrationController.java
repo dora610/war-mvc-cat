fbff11c3fae537e35f92a9adf03c6f560d764fdb
“Surajit <surajitkaruri@gmail.com>
2023-03-19 21:49:41 +0530

    post/redirect/get

--- src/main/java/com/mvcSpringBoot/conference/controller/RegistrationController.java
+++ src/main/java/com/mvcSpringBoot/conference/controller/RegistrationController.java
@@ -32,6 +32,6 @@ public class RegistrationController {
 			Map<String, String> model) {
 		logger.info(registration.getName());
 		model.put("successMessage", "Registration successful");
-		return "registration";
+		return "redirect:registration";
 	}
 }
